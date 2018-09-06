65e59579bab2dd0ff98c06e168064ac5687d68cd
Rodrigo Quezada <roderik.ikana@gmail.com>
2018-09-06 15:19:54 -0500

    feat: add file removal

--- R/main.R
+++ R/main.R
@@ -7,14 +7,15 @@
 
 
 ggs3plotly <- function(plotblob) {
+  temp_name <- "temp.html"
   plot <- plotly::ggplotly(plotblob)
-  html <- htmlwidgets::saveWidget(plot, "temp.html")
-  output_filename <- paste(uuid::UUIDgenerate(), ".html", sep = "")
+  html <- htmlwidgets::saveWidget(plot, temp_name)
+  output_filename <- paste("plots/" ,uuid::UUIDgenerate(), ".html", sep = "")
   bucket_name <- "julieta-static"
-  path <- aws.s3::put_object(file = "temp.html", object = output_filename, bucket = bucket_name, acl = "public-read")
+  path <- aws.s3::put_object(file = temp_name, object = output_filename, bucket = bucket_name, acl = "public-read")
 
   url <- paste("https://s3-", Sys.getenv('AWS_DEFAULT_REGION'), ".amazonaws.com/", bucket_name, "/", output_filename, sep = "")
-
+  if (file.exists(temp_name)) file.remove(temp_name)
   return(url)
 }
 
