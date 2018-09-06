ee9ab106c0387e8235649a1638a99a10df3b6ee9
Rodrigo Quezada <roderik.ikana@gmail.com>
2018-09-06 14:54:26 -0500

    fix: update used bucket

--- R/main.R
+++ R/main.R
@@ -9,7 +9,7 @@
 ggs3plotly <- function(plotblob) {
   plot <- plotly::ggplotly(plotblob)
   html <- htmlwidgets::saveWidget(plot, "temp.html")
-  path <- aws.s3::put_object(file = "temp.html", object = "iframe_plot_whatever.html", bucket = "caro-bucket-function", acl = "public-read")
+  path <- aws.s3::put_object(file = "temp.html", object = "iframe_plot_whatever.html", bucket = "julieta-static", acl = "public-read")
 
   url <- paste("https://s3-", Sys.getenv('AWS_DEFAULT_REGION'), ".amazonaws.com/", "caro-bucket-function/iframe_plot_whatever.html",sep = "")
 
